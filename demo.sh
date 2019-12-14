42afe1dfc579dc29cb4737b219c197a51d7a8b30
PKQ <zhutaonan@i-i.ai>
2019-12-15 03:47:42 +0800

    update

--- demo.sh
+++ demo.sh
@@ -5,5 +5,4 @@ experiments/seg_detector/rctw17_resnet50_deform_thre.yaml \
 --resume outputs/workspace/DB/SegDetectorModel-seg_detector/deformable_resnet50/L1BalanceCELoss/model/model_epoch_5_minibatch_1500 \
 --box_thresh 0.1 \
 --thresh 0.1 \
---polygon \
 --visualize
