727490153d88b476009eb421830e235a19d0f74b
PKQ <zhutaonan@i-i.ai>
2019-12-15 11:29:41 +0800

    update

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@
 CUDA_VISIBLE_DEVICES=6 python demo.py \
 experiments/seg_detector/rctw17_resnet50_deform_thre.yaml \
 --image_path images/miao_111.png \
---resume outputs/workspace/DB/SegDetectorModel-seg_detector/deformable_resnet50/L1BalanceCELoss/model/model_epoch_5_minibatch_1500 \
+--resume outputs/workspace/DB/SegDetectorModel-seg_detector/deformable_resnet50/L1BalanceCELoss/model/model_epoch_15_minibatch_4000 \
 --box_thresh 0.1 \
---thresh 0.1 \
+--thresh 0.6 \
 --visualize
