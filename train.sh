9873f39412870645bbc36db56427791c514c4831
PKQ <zhutaonan@i-i.ai>
2020-01-07 16:04:05 +0800

    update

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export CUDA_VISIBLE_DEVICES=1; python train.py \
 experiments/seg_detector/rctw17_resnet50_deform_thre.yaml \
---num_gpus 1 --batch_size 8 \
+--num_gpus 1 --batch_size 64 \
 --resume outputs/workspace/DB/SegDetectorModel-seg_detector/deformable_resnet50/L1BalanceCELoss_1215/model/final \
 --epochs 50 \
 --lr 0.0001
