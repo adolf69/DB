d605f72bdc27a1efc1f7756302ad9d4641b7df9c
PKQ <zhutaonan@i-i.ai>
2020-01-13 11:56:07 +0800

    update

--- demo.sh
+++ demo.sh
@@ -2,9 +2,9 @@
 CUDA_VISIBLE_DEVICES=0 python demo.py \
 experiments/seg_detector/rctw17_resnet50_deform_thre.yaml \
 --image_path images/miao2.png \
---resume outputs/workspace/DB/SegDetectorModel-seg_detector/deformable_resnet50/L1BalanceCELoss_x1000_369/model/final \
+--resume outputs/workspace/DB/SegDetectorModel-seg_detector/deformable_resnet50/L1BalanceCELoss/model/final \
 --visualize \
 --box_thresh 0.1 \
 --thresh 0.5 \
---IsResize \
---image_short_side 1600
+#--IsResize \
+#--image_short_side 1600
